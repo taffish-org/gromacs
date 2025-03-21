f92d32563c0c07a7d4a5b349bd0cbcf144ae4d4f
Kaiyuan Han <hermit_han@163.com>
2025-03-22 02:08:15 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# taf-gromacs
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/gromacs/gromacs
+
+A free and open-source software suite for high-performance molecular dynamics and output analysis.
---
+++ gromacs.info.lisp
@@ -0,0 +1,7 @@
+(:name           "gromacs")
+(:repo           "taffish-org/gromacs")
+(:author         "KaiyuanHan")
+(:version        "2025.1")
+(:intro          "A free and open-source software suite for high-performance molecular dynamics and output analysis.")
+(:dependencies)
+(:functions      "gmx")
